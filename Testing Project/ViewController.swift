f0de6e126f77e39eb38083824cf4dc59c864e9a7
Hubert Wang <hubert.tatra@gmail.com>
2018-08-12 14:44:46 +0700

    Initial Commit

---
+++ Testing Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing Project
+//
+//  Created by Hubert Wang on 12/08/18.
+//  Copyright © 2018 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
